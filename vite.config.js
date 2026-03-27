243b6e53a996e9bdcb9720837a71a13557355ec7
pugalarasan-5 <pugalarasan302@gmail.com>
2026-03-28 05:19:33 +0530

    tt

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",   // 👈 MUST MATCH repo name
+  base: "/portfolio/"   // ✅ IMPORTANT
 })
